6a4acb9a0db10e21eb3de0e2917179b9853c7cad
Nikolai Kozak <nikokozak@gmail.com>
2021-06-22 00:11:26 -0400

    tree creation now tokenizes all objects, resulting in a properly parsable tree

--- fs-utils.scm
+++ fs-utils.scm
@@ -1,35 +1,17 @@
-(module fs-utils (*)
+;; Functions for dealing with the filesystem by invoking sh commands.
+;(module fs-utils (*)
 
   (import scheme)
   (import (chicken base))
   (import (chicken format))
   (import shell)
+  (import records)
+  (import token)
 
-  ;; Functions for dealing with the filesystem by invoking sh commands.
 
-  ;; When used with a shell command such as (capture EXPRESSION),
-  ;; takes the result of the shell command and turns it into a list of strings.
-  ;; If the shell command fails, or does not return, the function returns an empty list.
-  (define (shell-command->list shell-command)
-    (let* ((command-result (if (eof-object? shell-command)
-                              ""
-                              shell-command))
-          (port (open-input-string command-result)))
-      (letrec ((reader (lambda ()
-                         (let ((current (read port)))
-                          (cond
-                            ((eof-object? current) '())
-                            (else
-                            (cons (prune-end-slash (symbol->string current)) (reader))))))))
-        (reader))))
+  (define image-formats '("jpg" "jpeg" "png" "tiff"))
+  (define image-folders '("images" "imgs"))
 
-  ;; Removes the final / from a non-empty string.
-  (define (prune-end-slash str)
-    (let ((penultimate (- (string-length str) 1)))
-      (if (equal? (string-ref str penultimate)
-                  #\/)
-          (substring str 0 penultimate)
-          str)))
 
   ;; When passed no args, returns the full path to the CWD.
   ;; When passed one or more args, concatenates the args onto the CWD.
@@ -42,7 +24,7 @@
           (letrec ((helper (lambda (arglist)
                             (if (not (null? arglist))
                                 (string-append
-                                  (if (not last?)
+                                  (if (not (last? arglist))
                                       (string-append (car arglist) "/")
                                       (car arglist))
                                   (helper (cdr arglist)))
@@ -50,31 +32,18 @@
             (string-append cwd (string-append "/" (helper args))))
           cwd)))
 
-  (define (_find charlist char index)
-    (cond
-     ((null? charlist) #f)
-     (else
-      (if (equal? (car charlist) char)
-        index
-        (_find (cdr charlist) char (add1 index))))))
-
-  (define (_strip-until-last char str)
-    (let ((reversed (reverse (string->list str))))
-      (substring str (- (string-length str) (_find reversed char 0)))))
-
-  (define (_strip-after-last char str)
-    (let ((reversed (reverse (string->list str))))
-      (substring str 0 (- (string-length str) (add1 (_find reversed char 0))))))
-
+  ;; Returns the proverbial file-name or folder-name of an absolute-path,
+  ;; where a folder in an absolute path is not terminated with "/"
   (define (path-end path)
     (_strip-until-last #\/ path))
 
+  ;; Removes any extension information from a filename.
   (define (strip-extension path)
     (_strip-after-last #\. path))
 
   ;; List the contents of a given directory
   ;; full? -> use full pathnames
-  (define (dir-contents path . full?)
+  (define (dir path . full?)
     (if (not (null? full?))
         (shell-command->list (capture (cd ,path && ls -d -1 \"$PWD/\"*)))
         (shell-command->list (capture (cd ,path && ls)))))
@@ -94,7 +63,7 @@
         (shell-command->list (capture (cd ,path && ls -d */)))))
 
   ;; Is the given path a directory?
-  (define (directory? path)
+  (define (dir? path)
     (let ((result (shell-command->list (capture (test -d ,path && echo \"true\")))))
       (if (not (null? result))
           (equal? (car result) "true")
@@ -107,41 +76,158 @@
           (equal? (car result) "true")
           #f)))
 
+  ;; Is the given path an image? compares with image-formats.
+  (define (image? path)
+    (and (file? path) (contains? (extension path) image-formats)))
+
   ;; Checks whether a given path exists
   (define (exists? path)
     (and (directory? path) (file? path)))
 
 
+  ;; Checks whether a given folder name is within the image-folders
+  (define (image-dir? dirname)
+    (or (contains? dirname image-folders)
+        (contains? (path-end dirname) image-folders)))
 
-  ;; A simple record structure to hold dir information.
-  (define-record dir name abs-path rel-path contents)
-  (set-record-printer! dir (lambda (x out)
-                            (fprintf out "(#dir ~S ~S ~S ~S)"
-                                     (dir-name x)
-                                     (dir-abs-path x)
-                                     (dir-rel-path x)
-                                     (dir-contents x))))
+;; Checks whether a list contains a given element
+  (define (contains? obj ls)
+    (foldr (lambda (x seed) (or x seed)) #f
+          (map (lambda (x) (or (equal? x obj) (string-ci=? x obj))) ls)))
 
-  (define-record file name type path)
+  ;; Returns a file's extension
+  (define (extension filename-or-path)
+    (_strip-until-last #\. filename-or-path))
 
-  ;; Returns a tree of dirs, including files and subdirs.
-  (define (tree path)
+  ;; Returns a pair (w h) or () if unable to read.
+  (define (img-dims path)
+    (shell-command->list
+    (capture (file ,path -b \|
+                    grep -Eo \'\[0-9\]+\\\s?x\\\s?\[0-9\]+\'
+                    \| grep -Eo \'\[0-9\]+\'))))
+
+  ;; Returns a file's size in kb or -1 if error
+  (define (file-size path)
+    (let ((result (shell-command->list (capture (du -k ,path)))))
+      (if (not (null? result))
+          (car result)
+          -1)))
+
+;; A Directory token
+;; (define a (Token name abs-path ...))
+;; (token-name a)
+;; (set! (token-name a) "new")
+;; (token-token? a)
+(make-token Dir
+            name
+            abs-path
+            rel-path
+            contents
+            image-dir?)
+
+;; A File token
+(make-token File
+            name
+            abs-path
+            rel-path
+            type)
+
+;; An image token
+(make-token Img
+            name
+            abs-path
+            rel-path
+            type
+            width
+            height
+            size)
+
+;; An error token
+(make-token Error
+            name
+            abs-path
+            rel-path)
+
+(define (make-tree path)
+  (let
+      ((start-dir path))
     (letrec
-        ((helper (lambda (dirs)
-                      (cond
-                      ((null? dirs) '())
-                      (else
-                        (let ((dir-name (path-end (car dirs)))
-                              (dir-path (car dirs))
-                              (files (dir-files (car dirs)))
-                              (other-dirs (cdr dirs)))
-                          (cons
-                            (make-dir
-                              dir-name
-                              dir-path
-                              (helper (dir-dirs dir-path #t)) ;; recurr
-                              files)
-                            (helper other-dirs))))))))
-      (helper (dir-dirs path #t))))
+        ((travel (lambda (objects rel-path)
+                   (if (null? objects)
+                       '()
+                       (let*
+                           ((obj-name (path-end (car objects)))
+                            (obj-abs-path (car objects))
+                            (obj-rel-path (string-append rel-path "/" obj-name))
+                            (obj-is-dir? (directory? obj-abs-path)))
+                         (cons
+                          (cond
+                           (obj-is-dir? (Dir obj-name
+                                             obj-abs-path
+                                             obj-rel-path
+                                             (travel (dir obj-abs-path 'abs-paths) obj-rel-path)
+                                             (image-dir? obj-name)))
+                           ((image? obj-abs-path) (Img obj-name
+                                                       obj-abs-path
+                                                       obj-rel-path
+                                                       (extension obj-name)
+                                                       (car (img-dims obj-abs-path))
+                                                       (cadr (img-dims obj-abs-path))
+                                                       (file-size obj-abs-path)))
+                           ((file? obj-abs-path) (File obj-name
+                                                       obj-abs-path
+                                                       obj-rel-path
+                                                       (extension obj-name)))
+                           (else (Error obj-name
+                                        obj-abs-path
+                                        obj-rel-path)))
+                          (travel (cdr objects) rel-path)))))))
+      (travel (dir start-dir 'abs-paths) ""))))
 
-)
+  ;; When used with a shell command such as (capture EXPRESSION),
+  ;; takes the result of the shell command and turns it into a list of strings.
+  ;; Takes care of minor formatting adjustments, like pruning end "/" in abs-paths.
+  ;; If the shell command fails, or does not return, the function returns an empty list.
+  (define (shell-command->list shell-command)
+    (let* ((command-result (if (eof-object? shell-command)
+                              ""
+                              shell-command))
+          (port (open-input-string command-result)))
+      (letrec ((reader (lambda ()
+                         (let ((current (read port)))
+                          (cond
+                            ((eof-object? current) '())
+                            (else
+                             (cons (if (symbol? current)
+                                       (prune-end-slash (symbol->string current))
+                                       current)
+                                   (reader))))))))
+        (reader))))
+
+  ;; Removes the final / from a non-empty string.
+  (define (prune-end-slash str)
+    (let ((penultimate (- (string-length str) 1)))
+      (if (equal? (string-ref str penultimate)
+                  #\/)
+          (substring str 0 penultimate)
+          str)))
+
+  ;; Find the index of a given character in a charlist, starting at index.
+  (define (_find charlist char index)
+    (cond
+    ((null? charlist) 0)
+    (else
+      (if (equal? (car charlist) char)
+          index
+          (_find (cdr charlist) char (add1 index))))))
+
+  ;; Strip all characters from string up until last occurrence of a given character.
+  (define (_strip-until-last char str)
+    (let ((reversed (reverse (string->list str))))
+      (substring str (- (string-length str) (_find reversed char 0)))))
+
+  ;; Strip all characters from string after last occurrence of a given character.
+  (define (_strip-after-last char str)
+    (let ((reversed (reverse (string->list str))))
+      (substring str 0 (- (string-length str) (add1 (_find reversed char 0))))))
+;)
---
+++ token.scm
@@ -0,0 +1,102 @@
+(module token ((make-token))
+
+  (import scheme)
+  (import (chicken base))
+  (import (chicken syntax))
+  (import (chicken format))
+  (import fs-utils)
+  (import records)
+
+  ;; ;; Takes a symbol and returns a fully capitalized version.
+  ;; (define (capitalize-symbol symb)
+  ;;   (string->symbol (list->string (map char-upcase (string->list (symbol->string symb))))))
+
+  ;; ;; Takes a symbol and returns a downcase version.
+  ;; (define (downcase-symbol symb)
+  ;;   (string->symbol (list->string (map char-downcase (string->list (symbol->string symb))))))
+
+  ;; Creates getter and setter templates for records.
+  (define-for-syntax (accessor-defs-for name struct fields)
+    (cond
+    ((null? fields) '())
+    (else
+      (cons
+      `(define ,(symbol-append name '- (car fields))
+          (getter-with-setter (record-accessor ,struct ',(car fields))
+                              (record-modifier ,struct ',(car fields))))
+      (accessor-defs-for name struct (cdr fields))))))
+
+  ;; Creates accessor templates for records. NOTE: the x in the accessor is hardcoded atm,
+  ;; this is cheeky and should be changed pronto.
+  (define-for-syntax (accessors-for name fields var)
+    (cond
+    ((null? fields) '())
+    (else
+      (cons
+      `(,(symbol-append name '- (car fields)) x)
+      (accessors-for name (cdr fields) var)))))
+
+  ;; Creates format string template for print function
+  (define-for-syntax (format-string name fields)
+    (let ((field-length (length fields)))
+      (letrec ((draw-s (lambda (fields-left)
+                        (if (= 0 fields-left)
+                            ""
+                            (string-append "~s " (draw-s (sub1 fields-left)))))))
+        (string-append "#(" (symbol->string name) " " (draw-s field-length) ")"))))
+
+  ;; Defines a new #Record
+  ;; eg. (make-token Test a b)
+  ;; eg. (define rec (Test a b ...))
+  ;; eg. (test-a rec) => a
+  ;; eg. (test-record? rec) => #t
+  ;; eg. (set! (test-a rec) 10) => 10
+  ;; rec => #(test "a" "b")
+  (define-syntax make-token
+    (er-macro-transformer
+    (lambda (expr replace compare?)
+      (let* ((capitalize-symbol (lambda (symb)
+                                  (string->symbol (list->string (map char-upcase (string->list (symbol->string symb)))))))
+             (downcase-symbol (lambda (symb)
+                                (string->symbol (list->string (map char-downcase (string->list (symbol->string symb)))))))
+             (name (cadr expr))
+             (upcase (capitalize-symbol (cadr expr)))
+             (downcase (downcase-symbol (cadr expr)))
+             (fields (cddr expr))
+             (token-printer `(lambda (x out)
+                               (fprintf out ,(format-string downcase fields)
+                                        ,@(accessors-for downcase fields 'x)))))
+
+        `(begin
+            ;; (define TEST (make-record-type test '(field1 field2 ...)))
+            (define ,upcase (make-record-type ',downcase '(,@fields)))
+            ;; (define Test (record-constructor TEST))
+            (define ,name (record-constructor ,upcase))
+            ;; (define test-token? (record-predicate TEST))
+            (define ,(symbol-append downcase '-token?) (record-predicate ,upcase))
+            ;; (define test-field1 (getter-with-setter TEST (record-accessor TEST 'test-field1) ...))
+            ,@(accessor-defs-for downcase upcase fields)
+            (set-record-printer! ',downcase ,token-printer))))))
+
+  ;; "Tokens"
+  ;; (make-token Dir
+  ;;             name
+  ;;             abs-path
+  ;;             rel-path
+  ;;             contents)
+
+  ;; (make-token File
+  ;;             name
+  ;;             type
+  ;;             abs-path
+  ;;             rel-path)
+
+  ;; (make-token Img
+  ;;             name
+  ;;             type
+  ;;             abs-path
+  ;;             rel-path
+  ;;             width
+  ;;             height
+  ;;             uid)
+)
